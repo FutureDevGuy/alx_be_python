3c79e5a90dbab79a5bf26221b8970488a8f9a92a
Philip Oseikhuemen Inegbedion <philliposeikhuemen@gmail.com>
2025-08-10 23:52:39 +0100

    Final attempt: Adding a fresh, verified book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -18,7 +18,6 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        print(f"Book '{self.title}' created.")
 
     def __del__(self):
         """
